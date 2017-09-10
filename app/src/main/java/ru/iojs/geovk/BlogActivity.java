cae098d2d29c1f2abe494b734ab301103f5a9451
Max Zaytsev <zaytsev@mail.com>
2017-09-10 17:46:31 +0300

    Change Activity logic

--- app/src/main/java/ru/iojs/geovk/BlogActivity.java
+++ app/src/main/java/ru/iojs/geovk/BlogActivity.java
@@ -1,14 +1,18 @@
 package ru.iojs.geovk;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by maksimus on 10.09.17.
+ */
+
+public class BlogActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_blog);
     }
-
 }
