774608350e10fdb52edaff45b3f515fe9fb354bb
Evfrosiniya <evfrosiniya.z@gmail.ru>
2017-09-10 18:18:31 +0300

    add blog activity

--- app/src/main/java/ru/iojs/geovk/BlogActivity.java
+++ app/src/main/java/ru/iojs/geovk/BlogActivity.java
@@ -1,12 +1,11 @@
 package ru.iojs.geovk;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by maksimus on 10.09.17.
- */
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class BlogActivity extends AppCompatActivity {
 
@@ -14,5 +13,16 @@ public class BlogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
