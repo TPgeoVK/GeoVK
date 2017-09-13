93852163cbcb2b87c1ceadb014ed7df9fd94fd4b
Max Zaytsev <zaytsev@mail.com>
2017-09-13 19:56:06 +0300

    Drawer changed

--- app/src/main/java/ru/iojs/geovk/RecommendationsActivity.java
+++ app/src/main/java/ru/iojs/geovk/RecommendationsActivity.java
@@ -1,5 +1,6 @@
 package ru.iojs.geovk;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -81,7 +82,8 @@ public class RecommendationsActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            Intent intent = new Intent(RecommendationsActivity.this, RecommendationsActivity.class);
+            startActivity(intent);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
