835e3289cb04942c81e53217bfc420ddef0e7520
Max Zaytsev <zaytsev@mail.com>
2017-09-14 15:17:00 +0300

    Optimize imports

--- app/src/main/java/ru/iojs/geovk/NewPostActivity.java
+++ app/src/main/java/ru/iojs/geovk/NewPostActivity.java
@@ -1,7 +1,7 @@
 package ru.iojs.geovk;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
 
