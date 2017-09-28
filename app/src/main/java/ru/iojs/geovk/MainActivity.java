0e89f81276f39f271a18225fc82e0294dec00151
Max Zaytsev <zaytsev@mail.com>
2017-09-28 17:54:38 +0300

    Frosya add the palka

--- app/src/main/java/ru/iojs/geovk/MainActivity.java
+++ app/src/main/java/ru/iojs/geovk/MainActivity.java
@@ -1,6 +1,5 @@
 package ru.iojs.geovk;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -14,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void login(View view) {
-        Intent intent = new Intent(MainActivity.this, RecommendationsActivity.class);
-        startActivity(intent);
+        //Intent intent = new Intent(MainActivity.this, RecommendationsActivity.class);
+        //startActivity(intent);
     }
 }
